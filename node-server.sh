ca9099c6ec86f086fe853efdbc19d02664eaad1e
Jian Earle <jianearle93@googlemail.com>
2020-06-25 12:35:15 +0100

    Updated the files created

--- node-server.sh
+++ node-server.sh
@@ -1,6 +1,6 @@
 mkdir node-server 
 cd node-server
-mkdir controllers middleware apis public 
-touch server.js 
+mkdir controllers middleware apis models routes public utils
+touch server.js .gitignore .env
 npm init -y
 npm i express body-parser mongoose 
